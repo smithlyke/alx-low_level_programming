6265dbe5da5992702fe00726c5d163a3d6dd1d78
anyaele <smithanyaele@gmail.com>
2023-06-26 19:25:14 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,14 +1,15 @@
-#include <studio.h>
-#include <unistd.h>
+#ifnef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void print_rev (char *s);
+void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int m);
-char *_strcpy(char *dest, char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
