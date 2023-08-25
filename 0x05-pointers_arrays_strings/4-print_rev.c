40d6cf9f060714095bd055bb7acbf9b76775fc42
smithlyke <smithanyaele@gmail.com>
2023-08-24 18:51:09 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,18 +9,16 @@ void print_rev(char *s)
 	int index = 0;
 	int longi;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		index++;
 		s++;
-	
 	}
 	s--;
 	for (longi = index; longi > 0; longi--)
 	{
 		_putchar(*s);
 		s--;
-	
 	}
 	_putchar('\n');
 }
